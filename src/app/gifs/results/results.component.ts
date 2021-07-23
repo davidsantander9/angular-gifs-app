7227c6e9d6e8fbc46d57317fa0b8f936df50112e
David <davidsantander9@gmail.com>
2021-07-23 17:09:32 -0500

    show results in html

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
@@ -6,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent{
 
-  constructor() { }
+  constructor(private gifsService:GifsService) { }
 
-  ngOnInit(): void {
+  get results(){
+    return this.gifsService.results;
   }
-
 }
