47defd94e33480b98604da8fbba732ccfaa26e4f
David <davidsantander9@gmail.com>
2021-07-23 17:49:08 -0500

    localstorage results

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,6 +14,7 @@ export class GifsService {
 
   constructor(private http: HttpClient) {
     this._history = JSON.parse(localStorage.getItem('history')!) || [] ;
+    this.results = JSON.parse(localStorage.getItem('results')!) || [] ;
   }
 
   get hystory(){
@@ -34,6 +35,7 @@ export class GifsService {
 
     this.http.get<SearchGifsResponse>(this.url).subscribe( (resp) => {
       this.results = resp.data;
+      localStorage.setItem('results', JSON.stringify(this.results))
     });
 
   }
