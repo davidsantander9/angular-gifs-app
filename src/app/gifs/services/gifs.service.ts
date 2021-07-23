a3d44850add7a8b04bbd3910b30dbb41450d33f6
David <davidsantander9@gmail.com>
2021-07-23 17:02:00 -0500

    make a http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'z8L84cRhMVVOIbQuduH0R8OxY88PESaX';
+  private url = '';
   private _history: string [] = [];
+  public results: any[] = [];
+
+  constructor(private http: HttpClient) {}
 
   get hystory(){
     return [...this._history];
@@ -16,13 +22,15 @@ export class GifsService {
     query = query.trim().toLowerCase();
 
     if ( !this._history.includes(query) ) {
-      
       this._history.unshift( query );
+      this._history = this._history.splice(0, 10);
+      this.url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI( query )}&limit=10&api_key=${this.apiKey}`;
     }
 
-  
+    this.http.get(this.url).subscribe( (resp: any) => {
+      this.results = resp.data;
+    });
 
-    this._history = this._history.splice(0, 10);
   }
 
 
